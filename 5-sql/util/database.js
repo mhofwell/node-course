9e6b1f9df6e728b550fae259af1381eae69e1174
Michael Hofweller <michael.hofweller@gmail.com>
2023-07-29 13:25:27 -0700

    updates

--- 5-sql/util/database.js
+++ 5-sql/util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-course', 'root', 'Jabba1234!', {
-    dialect: 'mysql',
-    host: 'localhost',
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
 module.exports = sequelize;
