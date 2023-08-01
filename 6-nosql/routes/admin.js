bcadb7c827062bafd8185b23ed31c0550a4bfbc8
Michael Hofweller <michael.hofweller@gmail.com>
2023-08-01 13:46:31 -0400

    updates

--- 6-nosql/routes/admin.js
+++ 6-nosql/routes/admin.js
@@ -1,9 +1,5 @@
-const path = require('path');
-
 const express = require('express');
-
 const adminController = require('../controllers/admin');
-
 const router = express.Router();
 
 // /admin/add-product => GET
