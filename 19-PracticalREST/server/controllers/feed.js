412354ea9eba5807fb702e4bf6f0c7094599dc26
Michael Hofweller <michael.hofweller@gmail.com>
2023-08-13 10:23:09 -0400

    PUT and DELETE for posts plus pagination

--- 19-PracticalREST/server/controllers/feed.js
+++ 19-PracticalREST/server/controllers/feed.js
@@ -1,13 +1,28 @@
 const { validationResult } = require('express-validator');
 const Post = require('../models/post');
+const fs = require('fs');
+const path = require('path');
 
 exports.getPosts = (req, res, next) => {
+    // implement pagination
+    const currentPage = req.query.page || 1;
+    // posts per page
+    const perPage = 2;
+    let totalItems;
     Post.find()
-        .then((posts) => {
-            res.status(200).json({
-                message: 'Fetched posts successfully',
-                posts: posts,
-            });
+        .countDocuments()
+        .then((count) => {
+            totalItems = count;
+            return Post.find()
+                .skip((currentPage - 1) * perPage)
+                .limit(perPage)
+                .then((posts) => {
+                    res.status(200).json({
+                        message: 'Fetched posts successfully',
+                        posts: posts,
+                        totalItems: totalItems,
+                    });
+                });
         })
         .catch((err) => {
             if (!err.statusCode) {
@@ -92,4 +107,92 @@ exports.getPost = (req, res, next) => {
             // to go to the next error handling software if you're at the end of the promise chain.
             next(err);
         });
+};
+
+exports.updatePost = (req, res, next) => {
+    const postId = req.params.postId;
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        // server side validation for length of content and title
+        const err = new Error('Server Response: Validation failed');
+        err.statusCode = 422;
+        // you can see this error thrown in your network tab. The red response has a payload you can inspect.
+        // throw err will automatically go to the next error handling middleware in express
+        throw err;
+    }
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+
+    if (req.file) {
+        imageUrl = req.file.path;
+    }
+
+    if (!imageUrl) {
+        const err = new Error('No file picked');
+        err.statusCode = 422;
+        throw err;
+    }
+    // now we know the data is valid. Now we update.
+    Post.findById(postId)
+        .then((post) => {
+            if (!post) {
+                const error = new Error('Could not find post');
+                error.statusCode = 404;
+                // throw to catch it
+                throw err;
+            }
+            // checking for imageURL change and then deleting the old imageUrl
+            if (imageUrl !== post.imageUrl) {
+                clearImage(post.imageUrl);
+            }
+            // we found a post, now we can update
+            post.title = title;
+            post.imageUrl = imageUrl;
+            post.content = content;
+            return post.save();
+        })
+        .then((result) => {
+            // return data to client in a response.
+            res.status(200).json({ message: 'Post updated', post: result });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            // to go to the next error handling software if you're at the end of the promise chain.
+            next(err);
+        });
+};
+
+exports.deletePost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then((post) => {
+            if (!post) {
+                const error = new Error('Could not find post');
+                error.statusCode = 404;
+                // throw to catch it
+                throw err;
+            }
+            // Check logged in user
+            clearImage(post.imageUrl);
+            return Post.findByIdAndRemove(postId);
+        })
+        .then((result) => {
+            console.log(result);
+            res.status(200).json({ message: 'Deleted Post ' + postId });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            // to go to the next error handling software if you're at the end of the promise chain.
+            next(err);
+        });
+};
+
+const clearImage = (filePath) => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, (err) => console.log(err));
 };
